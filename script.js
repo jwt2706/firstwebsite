a2817f6e6190bd2884d44c058fc3a8ca0774ad8d
Jacob T <55862563+jwt2706@users.noreply.github.com>
2021-07-02 17:10:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var test= true;
+var scrollval= document.body.scrollTop;
+//console.log(scrollval);
+
+
+for (let i = 0; i < 500; i++) {
+  scrollval= document.body.scrollTop;
+  console.log(scrollval);
+}
+
+
