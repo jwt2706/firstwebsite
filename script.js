2e6c9a5c024cfae8802af8887e908c8321570ca5
Jacob T <55862563+jwt2706@users.noreply.github.com>
2022-11-28 15:45:17 +0000

    import

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-var test= true;
-var scrollval= document.body.scrollTop;
-//console.log(scrollval);
+$(document).on("scroll", function() {
+  var pageTopVal = $(document).scrollTop();
+  var pageBottomVal = pageTopVal + $(window).height();
+  var logos = $(".logo");
+
+  for (var i = 0; i < logos.length; i++) {
+
+    if ($(logos[i]).position().top < pageBottomVal) {
+      $(logos[i]).addClass("visible");
+    } else {
+      $(logos[i]).removeClass("visible");
+    }
+  }
+});
 
 
-for (let i = 0; i < 500; i++) {
-  scrollval= document.body.scrollTop;
-  console.log(scrollval);
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
 }
-
-
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
